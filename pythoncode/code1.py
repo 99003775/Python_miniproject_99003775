b96fb5193ee9d9781fe1e9b6447bee4cd72f4563
99003775 <78869692+99003775@users.noreply.github.com>
2021-02-25 12:41:57 +0530

    for loop inserted

--- pythoncode/code1.py
+++ pythoncode/code1.py
@@ -2,14 +2,16 @@
 # Ps No:99003775
 # Date 02/25/2021
 import re  # import Regular Expression module
+    
+cycle=int(input("enter number of word"))
 
-
+ 
 class Code:  # Class Name
     def __init__(self):  # To initilize obj It is constracter
         self.word = input("Enter keyword\n")  # To take user input
         self.Output_file_name = self.word+".txt"  # to make txt file
         self.Input_file = open("input.txt", 'r')  # open input file in read mode
-        self.Output_file = open(self.Output_file_name, "w")  # open file in write mode
+        self.Output_file = open(self.Output_file_name, "w")  # open output file in write mode
         self.count = 0  # initilize count variable
         
     def Get_Input(self):   # meathod to read  file text
@@ -30,7 +32,8 @@ class Code:  # Class Name
     def Shut_file(self):
         self.Input_file.close()  # closing input file
         self.Output_file.close()  # closing outputfile
-myobj = Code()  # create object
-file1 = myobj.Get_Input()  # call Get_input meathod
-myobj.Match_Input(file1)  # call Match_input meathod
-myobj.Shut_file()   # close file
+for loop in range(cycle):
+    myobj = Code()  # create object
+    file1 = myobj.Get_Input()  # call Get_input meathod
+    myobj.Match_Input(file1)  # call Match_input meathod
+    myobj.Shut_file()   # close file
